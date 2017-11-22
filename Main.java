a688000a047cb115ee5a32fb680f4c34de55aa99
Thieskeloo <thijsvanloo4@gmail.com>
2017-11-22 16:49:48 +0100

    added neuron

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
-public class Main {
+package perceptron;
 
-    public static void main(String[] args) {
-    int[] input = new int[2];
+public class main {
 
-    }
+	public static void Main(String[] args) {
+		Perceptron p = new Perceptron();
+		double inputs[][] = {{0,0},{0,1},{1,0},{1,1}};
+		int outputs[] = {0,0,0,1};
+
+		p.Train(inputs, outputs,0.2, 0.1, 200);
+		System.out.println(p.Output(new double[]{0,0}));
+		System.out.println(p.Output(new double[]{1,0}));
+		System.out.println(p.Output(new double[]{0,1}));
+		System.out.println(p.Output(new double[]{1,1}));
+
+	}
+
 }
