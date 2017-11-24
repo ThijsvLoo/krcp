a84e51926be4cd9e0de891739ebef87e707564d5
Thieskeloo <thijsvanloo4@gmail.com>
2017-11-24 11:03:21 +0100

    added neuron

--- Neuron.java
+++ Neuron.java
@@ -2,9 +2,38 @@ import java.util.Random;
 
 public class Perceptron {
 
-	double[] weights;
-	double threshold;
+	private double[] weights;
+	private double threshold;
+	private double threshold;
+	private weights = new double[n];
+	private Random r = new Random();
 
+	public Perceptron(){
+		//initialize weights
+		for(int i=0;i<n;i++)
+		{
+			weights[i] = r.nextDouble();
+		}
+
+		threshold = 1;
+	}
+
+	public void Train(double[][] inputs, int[] outputs, double lrate,)
+	{
+		int n = inputs[0].length;
+		int p = outputs.length;
+
+		while(error > 0.5){
+			for(i = 0; i < intputs.length; i++){
+
+			}
+		}
+
+	}
+
+	private int calculateActivation(int[][] inputs, ){
+
+	}
 
 	public int Output(double[] input) {
 		double sum = 0.0;
