47d454da150ef9c4e6860df5517860a14c3383ab
StijnCoppens <styncoppens@gmail.com>
2017-11-29 12:11:25 +0100

    k

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		double[][] input = {{0,0},{0,1},{1,0},{1,1}};
-		int[] output = {0,0,0,1};
+		int[] output = {0,1,0,1};
 		Network network = new Network();
 		network.Train(input, output);
 		for (int i = 0; i<input.length;i++)
